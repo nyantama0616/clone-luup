24dbebf3999a9597f48d681c09cfa10d291656f8
中北竜馬 <b1020183@fun.ac.jp>
2024-05-07 19:15:18 +0900

    LuupBorderのwidthを長くしてみた
    Displayのwidthが長いと、LuupBorderのwidthが足りなくなるため

--- src/commons/components/atoms/LuupBorder/LuupBorder.tsx
+++ src/commons/components/atoms/LuupBorder/LuupBorder.tsx
@@ -1,7 +1,7 @@
 import GeneralProps2 from "@/commons/types/GeneralProps2";
 
 function LuupBorder({ className }: GeneralProps2) {
-    const _className = `luup-border w-[800px] h-[280px] border border-primary-200 border-[35px] rounded-[50px] ${className}`;
+    const _className = `luup-border w-[1100px] h-[280px] border border-primary-200 border-[35px] rounded-[50px] ${className}`;
     return (
         <div className={_className}>
 
