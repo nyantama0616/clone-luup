81d2f5c0b6344193877c7a10329d0cd52864f4f4
中北竜馬 <b1020183@fun.ac.jp>
2024-05-08 13:27:48 +0900

    TextFieldコンポーネントを作成

---
+++ src/commons/components/molecules/Forms/TextField/TextField.stories.ts
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import TextField from './TextField';
+
+const meta = {
+    title: 'Commons/Molecules/Forms/TextField',
+    component: TextField,
+    parameters: {
+        layout: 'centered',
+        design: {},
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof TextField>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        label: 'ラベル',
+        required: true,
+        placeholder: "プレースホルダー",
+        className: "w-[960px]",
+    }
+};
+
+export const Email: Story = {
+    args: {
+        label: 'メールアドレス',
+        required: true,
+        placeholder: "info@luup.sc",
+        className: "w-[960px]",
+    }
+};
+
+export const Name: Story = {
+    args: {
+        label: 'お名前',
+        required: true,
+        placeholder: "山田太郎",
+        className: "w-[960px]",
+    }
+};
---
+++ src/commons/components/molecules/Forms/TextField/TextField.tsx
@@ -0,0 +1,28 @@
+import GeneralProps2 from "@/commons/types/GeneralProps2";
+
+interface TextFieldProps extends GeneralProps2 {
+    label: string;
+    required?: boolean;
+    placeholder?: string;
+    name?: string;
+}
+
+// TODO: フォーカス時の色を変えたい
+function TextField({ label, required = false, placeholder="", className="", name, }: TextFieldProps) {
+    const requiredTag = required ? <span className="bg-black text-white text-xs rounded-md w-[50px] text-center px-2 py-1">必須</span> : null;
+    const _className = `grid grid-cols-4 gap-8 items-center ${className}`;
+    const _placeholder = `例）${placeholder}`;
+
+    return (
+        <label className={_className}>
+            <span className="block text-base font-medium text-gray-700">{label}</span>
+            
+            <div className="col-span-3 flex items-center space-x-4">
+                {requiredTag}
+                <input type="text" placeholder={_placeholder} required={required} name={name} className="grow h-[56px] px-3 py-2 border border-gray-200 rounded-md placeholder-gray-200 focus:ring-red focus:border-red" />
+            </div>
+        </label>
+    );
+}
+
+export default TextField;
