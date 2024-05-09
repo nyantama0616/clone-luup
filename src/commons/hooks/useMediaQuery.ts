5a1a990e363df437eac99daf03ef9cf9bba43153
中北竜馬 <b1020183@fun.ac.jp>
2024-05-09 16:30:29 +0900

    レスポンシブ対応用のフックを作成

---
+++ src/commons/hooks/useMediaQuery.ts
@@ -0,0 +1,35 @@
+/*
+    参考
+    https://zenn.dev/nabeliwo/articles/89099b39223eca
+*/
+
+import { useEffect, useState } from 'react'
+
+export const mediaQuery = {
+    sp: 'width < 752px',
+    // tablet: '752px <= width < 1122px',
+    pc: '1122px <= width',
+}
+
+export const useMediaQuery = (query: string) => {
+    const formattedQuery = `(${query})`
+    const [match, setMatch] = useState(matchMedia(formattedQuery).matches)
+
+    useEffect(() => {
+        const mql = matchMedia(formattedQuery)
+
+        if (mql.media === 'not all' || mql.media === 'invalid') {
+            console.error(`useMediaQuery Error: Invalid media query`)
+        }
+
+        mql.onchange = (e) => {
+            setMatch(e.matches)
+        }
+
+        return () => {
+            mql.onchange = null
+        }
+    }, [formattedQuery, setMatch])
+
+    return match
+}
