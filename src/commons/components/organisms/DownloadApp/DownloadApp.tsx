ad6a21232463816cf500247304101746e8b4b16a
中北竜馬 <b1020183@fun.ac.jp>
2024-05-11 00:37:08 +0900

    DownloadAppをスマホ対応させた

--- src/commons/components/organisms/DownloadApp/DownloadApp.tsx
+++ src/commons/components/organisms/DownloadApp/DownloadApp.tsx
@@ -5,21 +5,21 @@ import LinkButtonImage from "../../atoms/LinkButtonImage/LinkButtonImage";
 
 function AppDownload() {
     return (
-        <div className="flex justify-center py-12 bg-white">
-            <div className="grid grid-cols-5 gap-8 w-[1100px]">
+        <div className="flex justify-center px-6 py-12 bg-white">
+            <div className="md:grid md:grid-cols-5 md:gap-8 md:w-[1100px]">
                 <MyImage
-                    className="col-span-3"
+                    className="md:col-span-3"
                     info={ImageInfos.DownloadApp}
                 />
 
-                <div className="col-span-2 flex flex-col space-y-8 justify-center">
-                    <h1 className="text-3xl">アプリのダウンロードはこちら</h1>
+                <div className="mt-6 md:mt-0 col-span-2 flex flex-col space-y-6 md:space-y-8 justify-center">
+                    <h1 className="text-xl md:text-3xl">アプリのダウンロードはこちら</h1>
                     <div className="flex space-x-6 items-center">
                         <LinkButtonImage imageInfo={ImageInfos.AppStore.changeWidth(120)}/>
                         <LinkButtonImage imageInfo={ImageInfos.GooglePlay.changeWidth(150)}/>
                     </div>
 
-                    <p className="text-gray-300 text-xs">
+                    <p className="leading-loose text-gray-300 text-xs">
                         Apple および Apple ロゴは米国その他の国で登録された
                         Apple Inc. の商標です。<br />
                         App Store は Apple Inc. のサービスマークです。<br />
