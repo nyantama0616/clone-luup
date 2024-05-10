665870946b16622103a58d0fb4fba708a5b900a9
中北竜馬 <b1020183@fun.ac.jp>
2024-05-10 23:30:50 +0900

    Serviceをスマホ対応にした

--- src/commons/components/organisms/Service/Service.tsx
+++ src/commons/components/organisms/Service/Service.tsx
@@ -6,32 +6,44 @@ import AwesomeButton from "./AwesomeButton";
 import { useDesignContext } from "@/commons/contexts/DesignContext";
 import { useState } from "react";
 import LinkButtonWithImage from "../../atoms/LinkButtonWithImage/LinkButtonWithImage";
+import { useMediaQuery, mediaQuery } from "@/commons/hooks/useMediaQuery";
 
 function Service() {
     const { showService, serviceRef } = useDesignContext();
+    const isMobile = useMediaQuery(mediaQuery.sp);
+
+    const images = isMobile ? {
+        about: ImageInfos.ServiceAboutSp.changeWidth(480),
+        mobility: ImageInfos.ServiceMobilitySp.changeWidth(480),
+        usage: ImageInfos.ServiceUsageSp.changeWidth(480),
+    } : {
+        about: ImageInfos.ServiceAbout,
+        mobility: ImageInfos.ServiceMobility,
+        usage: ImageInfos.ServiceUsage,
+    };
     
     return (
-        <div className="w-full flex flex-col space-y-8 items-center" ref={serviceRef}>
+        <div className="w-full flex flex-col space-y-8 md:items-center" ref={serviceRef}>
             <GhostText
                 text="Service"
-                className="text-8xl"
+                className="pl-8 md:pl-0 text-6xl md:text-8xl"
                 appear={showService}
             />
 
-            <h3 className="text-2xl">サービス</h3>
+            <h3 className="pl-8 md:pl-0 text-xl md:text-2xl">サービス</h3>
 
-            <div className="flex">
-                <ServiceItem imageInfo={ImageInfos.ServiceAbout} text="サービスについて" />
-                <ServiceItem imageInfo={ImageInfos.ServiceMobility} text="モビリティ紹介" />
-                <ServiceItem imageInfo={ImageInfos.ServiceUsage} text="LUUPのご利用方法" borderRight/>
+            <div className="flex flex-col md:grid md:grid-cols-3">
+                <ServiceItem imageInfo={images.about} text="サービスについて" />
+                <ServiceItem imageInfo={images.mobility} text="モビリティ紹介" />
+                <ServiceItem imageInfo={images.usage} text="LUUPのご利用方法" borderRight/>
             </div>
 
             {/* TODO: 親要素のspace-y-8のせいで子要素にmarginを指定できないため、divで囲っいる */}
-            <div>
+            <div className="p-8">
                 <LinkButtonWithImage
                     label="新しい交通ルールについて"
                     imageInfo={ImageInfos.NewTraffic}
-                    className="w-[480px]"
+                    className="w-full md:w-[480px]"
                 />
             </div>
         </div>
@@ -55,7 +67,7 @@ function ServiceItem({ imageInfo, text, borderRight = false }: ServiceItemProps)
             <div className="flex flex-col space-y-4" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <ImageZoomable
                     imageInfo={imageInfo}
-                    className="w-[740px] h-[450px]"
+                    className="w-full h-[240px] md:h-[450px]"
                     isHovered={isHovered}
                 />
                 <AwesomeButton label={text} isHovered={isHovered} />
