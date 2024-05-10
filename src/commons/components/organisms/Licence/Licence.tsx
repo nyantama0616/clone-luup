ff47993c5093b78c0835eb7ae860a2e05bf67ab2
中北竜馬 <b1020183@fun.ac.jp>
2024-05-11 00:59:39 +0900

    Licenceをスマホ対応させた

--- src/commons/components/organisms/Licence/Licence.tsx
+++ src/commons/components/organisms/Licence/Licence.tsx
@@ -1,6 +1,6 @@
 function Licence() {
     return (
-        <div className="w-[1100px] flex justify-between text-xs pb-8">
+        <div className="px-8 w-full md:w-[1100px] flex flex-col space-y-2 md:space-y-0 md:flex-row justify-between text-xs pb-8">
             <div className="flex space-x-4">
                 <a href="#" className="hover:underline">利用規約</a>
                 <a href="#" className="hover:underline">プライバシーポリシー</a>
